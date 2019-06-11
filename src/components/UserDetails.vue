1db9c5cfa26acfa5f7bb565753b550d1063fcb6e
anna_curly <curly160795@gmail.com>
2019-06-11 13:56:44 +0300

    UserDeatails component was added

---
+++ src/components/UserDetails.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="details container">
+      <h1 class="page-header">User Details</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'userdetails',
+  data () {
+    return {
+    
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
